bd4b180d65f5f48793f3f29229e68a48f83c98e2
denis <denis@kaizen-design.ru>
2021-11-03 01:55:33 +0300

    GH pages and Sass configured

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import 'jquery';
-//import 'popper.js/dist/umd/popper';
 import 'bootstrap/dist/js/bootstrap';
-import 'bootstrap/dist/css/bootstrap.css'
-import './index.css';
-//import App from './App';
+import './scss/App.scss';
 import reportWebVitals from './reportWebVitals';
 import Navbar from './App';
 
