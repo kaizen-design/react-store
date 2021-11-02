9772af56cdcbd65940354e5ebd7909c8f2c8093b
denis <denis@kaizen-design.ru>
2021-11-03 00:04:34 +0300

    Bootstrap assets added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'jquery';
+import 'popper.js/dist/umd/popper';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
